874e8f1cedca67cb4755dcb5f8f78e53b798cc11
pratibha2001 <540pratibhacse1@gmail.com>
2023-09-01 16:59:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+
+const app=express();
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+let newItems=[];
+app.get('/',(req,res)=>{
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let today  = new Date();
+    let day=today.toLocaleDateString("en-US", options);
+    res.render("list",{kindOfDay:day,newListItems:newItems});
+});
+
+app.post('/',(req,res)=>{
+    let newItem=req.body.newItem;
+     newItems.push(newItem);
+    res.redirect('/');
+});
+
+app.listen(3000,()=>console.log("Port is running at server 3000"));
